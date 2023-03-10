04c47a4c2b6f02e0e42bfe0dcc66c2380d1cd48d
Stanislav Vitkosvkiy <Stasvitkovskiy700@gmail.com>
2023-03-10 17:30:10 +0300

    Fix

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -7,33 +7,33 @@ public class   Stylish {
     public static String format(List<Map<String, Object>> diffData) {
         StringBuilder builder = new StringBuilder("{\n");
         for (Map<String, Object> row : diffData) {
-            builder.append("    ");
+            builder.append("  ");
             if (row.get("operation").equals("add")) {
                 builder.append("+ ");
                 builder.append(row.get("key"));
-                builder.append(" : ");
+                builder.append(": ");
                 builder.append(row.get("newValue"));
             } else if (row.get("operation").equals("delete")) {
                 builder.append("- ");
                 builder.append(row.get("key"));
-                builder.append(" : ");
+                builder.append(": ");
                 builder.append(row.get("oldValue"));
             } else if (row.get("operation").equals("update")) {
                 builder.append("- ");
                 builder.append(row.get("key"));
-                builder.append(" : ");
+                builder.append(": ");
                 builder.append(row.get("oldValue"));
                 builder.append("\n");
 
                 builder.append("    ");
                 builder.append("+ ");
                 builder.append(row.get("key"));
-                builder.append(" : ");
+                builder.append(": ");
                 builder.append(row.get("newValue"));
             } else {
                 builder.append("  ");
                 builder.append(row.get("key"));
-                builder.append(" : ");
+                builder.append(": ");
                 builder.append(row.get("oldValue"));
             }
             builder.append("\n");
