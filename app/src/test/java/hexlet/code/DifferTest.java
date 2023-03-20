8c18864df94ee070c14e6144fe9b8986319f5b8f
Stanislav Vitkosvkiy <Stasvitkovskiy700@gmail.com>
2023-03-20 20:15:25 +0300

    Third part of fixes

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,21 +3,20 @@ package hexlet.code;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Stream;
 
 
 public class DifferTest {
-    private static String resultJSON;
-    private static String resultPlain;
-    private static String resultStylish;
-    private static String firstFileJSONPath;
-    private static String secondFileJSONPath;
-    private static String firstFileYAMLPath;
-    private static String secondFileYAMLPath;
+    private static Map<String, String> expectedResults;
 
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName).toAbsolutePath().normalize();
@@ -30,53 +29,38 @@ public class DifferTest {
 
     @BeforeAll
     public static void beforeAll() throws Exception {
-        firstFileJSONPath = getFixturePath("data1.json").toString();
-        secondFileJSONPath = getFixturePath("data2.json").toString();
-        firstFileYAMLPath = getFixturePath("data1.yaml").toString();
-        secondFileYAMLPath = getFixturePath("data2.yml").toString();
-
-        resultJSON = readFixture("result_json.json");
-        resultPlain = readFixture("result_plain.txt");
-        resultStylish = readFixture("result_stylish.txt");
+        expectedResults = Map.of("json", readFixture("result_json.json"),
+                        "plain", readFixture("result_plain.txt"),
+                        "stylish", readFixture("result_stylish.txt"),
+                        "default", readFixture("result_stylish.txt"));
     }
 
-    @Test()
-    public void compareJSONAsStylishTest() {
-        try {
-            String result = Differ.generate(firstFileJSONPath, secondFileJSONPath, "stylish");
-            assertEquals(resultStylish, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private static Stream<Arguments> getFileFormatAndOutputFormat() {
+        return Stream.of(
+                arguments("yml", "json"),
+                arguments("yml", "stylish"),
+                arguments("yml", "plain"),
+                arguments("yml", "default"),
+                arguments("yaml", "json"),
+                arguments("yaml", "stylish"),
+                arguments("yaml", "plain"),
+                arguments("yaml", "default"),
+                arguments("json", "json"),
+                arguments("json", "stylish"),
+                arguments("json", "plain"),
+                arguments("json", "default")
+        );
     }
 
-    @Test()
-    public void compareYAMLAsStylishTest() {
-        try {
-            String result = Differ.generate(firstFileYAMLPath, secondFileYAMLPath, "stylish");
-            assertEquals(resultStylish, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    @ParameterizedTest
+    @MethodSource("getFileFormatAndOutputFormat")
+    public void generateTest(String fileFormat, String outputFormat) throws Exception {
+        String filePath1 = getFixturePath("data1." + fileFormat).toString();
+        String filePath2 = getFixturePath("data2." + fileFormat).toString();
 
-    @Test()
-    public void compareJSONAsPlainTest() {
-        try {
-            String result = Differ.generate(firstFileJSONPath, secondFileJSONPath, "plain");
-            assertEquals(resultPlain, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test()
-    public void compareJSONAsJSONTest() {
-        try {
-            String result = Differ.generate(firstFileJSONPath, secondFileJSONPath, "json");
-            assertEquals(resultJSON, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String result = outputFormat.equals("default")
+                ? Differ.generate(filePath1, filePath2)
+                : Differ.generate(filePath1, filePath2, outputFormat);
+        assertEquals(expectedResults.get(outputFormat), result);
     }
 }
